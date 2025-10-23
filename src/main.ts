9af8ec9226ed41f773445671211d51784bcbc3f8
mahmoud mohamed <mahmoud.abdelbadea567@gmail.com>
2025-10-23 14:17:44 +0300

    Re-enable JSON and URL-encoded body parsing middleware in main.ts and add raw body parsing for webhook handling to support larger payloads and improve webhook processing.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
+import * as express from 'express';
 // const p = Promise.resolve(new Date()).then((res) => {console.log(res.toISOString())});
 // console.log(p);
 // console.log(new Date().getTimezoneOffset());
@@ -20,8 +21,9 @@ console.log(new Date().getHours());
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(json({ limit: '10mb' }));
-  // app.use(urlencoded({ extended: true, limit: '10mb' }));
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+  app.use('/payments/webhook', express.raw({ type: '*/*' }));
   // console.log('app',app);
   await app.listen(process.env.PORT ?? 3000);
 }
